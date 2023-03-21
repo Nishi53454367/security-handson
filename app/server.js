3b062ee8887356c83469b0f0c00ea6572c7c23a8
Naohiro Ishihara <nishi@NaohironoMacBook-Pro.local>
2023-03-21 13:46:19 +0900

    静的ファイルを配信するように変更

--- app/server.js
+++ app/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// 静的ファイルを配信
+app.use(express.static("public"));
+
 app.get('/', (req, res, next) => {
   res.end("Top Page");
 });
